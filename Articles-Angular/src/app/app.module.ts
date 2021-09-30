8f12b4e80f6ca321d9d7d9eba52040519f97df04
joniheri <jonheri001@gmail.com>
2021-09-30 21:37:25 +0700

    Install Material UI for Angular in /Article-Angular

--- Articles-Angular/src/app/app.module.ts
+++ Articles-Angular/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegisterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
